b1ceded9c6881dc84eaddc19889ff0678011ee5f
Beck <becksons@bu.edu>
2025-02-14 11:17:10 -0500

    Still fixing deployment issues...ahhhhh pt3

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.module.js';
-
-import PreloadScene from 'https://github.com/becksons/my-portfolio-game/blob/2c7f42f198fc404121e8d2943a04dc9d68c68583/PreloadScene.js';
-import TitleScene from 'https://github.com/becksons/my-portfolio-game/blob/2c7f42f198fc404121e8d2943a04dc9d68c68583/TitleScene.js';
-import GameScene from 'https://github.com/becksons/my-portfolio-game/blob/2c7f42f198fc404121e8d2943a04dc9d68c68583/GameScene.js';
-import ResumeScene from 'https://github.com/becksons/my-portfolio-game/blob/2c7f42f198fc404121e8d2943a04dc9d68c68583/ResumeScene.js';
-import NewWorldScene from 'https://github.com/becksons/my-portfolio-game/blob/2c7f42f198fc404121e8d2943a04dc9d68c68583/NewWorldScene.js';
+import Phaser from 'phaser';
+import PreloadScene from './PreloadScene.js';
+import TitleScene from './TitleScene.js';
+import GameScene from './GameScene.js';
+import ResumeScene from './ResumeScene.js';
+import NewWorldScene from './NewWorldScene.js';
 
 const config = {
-  parent: 'game',
   type: Phaser.AUTO,
   width: 2500,  
-  height: window.innerHeight > 1500 ? window.innerHeight - 1500 : 800,  
+  height: window.innerHeight-1500,
   physics: {
     default: 'arcade',
     arcade: {
@@ -23,6 +21,6 @@ const config = {
 const game = new Phaser.Game(config);
 
 window.addEventListener('resize', () => {
-  game.scale.resize(2500, window.innerHeight - 1500); // Keep the width fixed
+  game.scale.resize(2500, window.innerHeight-1500); // Keep the width fixed
   game.scale.refresh();
 });
