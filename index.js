bbc7f80fcf8ce8b087d14af14d99577fe5c0bfb1
Beck <becksons@bu.edu>
2025-02-14 10:41:35 -0500

    Still fixing deployment issues...

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import Phaser from 'phaser';
+import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.module.js';
+
 import PreloadScene from './PreloadScene.js';
 import TitleScene from './TitleScene.js';
 import GameScene from './GameScene.js';
@@ -6,6 +7,7 @@ import ResumeScene from './ResumeScene.js';
 import NewWorldScene from './NewWorldScene.js';
 
 const config = {
+  parent: 'game',
   type: Phaser.AUTO,
   width: 2500,  
   height: window.innerHeight > 1500 ? window.innerHeight - 1500 : 800,  physics: {
