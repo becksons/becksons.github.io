1e24bfd43b5b4a1a721694bb0233e273965af329
Beck <becksons@bu.edu>
2025-04-16 16:54:03 -0400

    fixing still :/

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+    base: '/becksons.github.io/',
     build: {
         rollupOptions: {
             output: {
